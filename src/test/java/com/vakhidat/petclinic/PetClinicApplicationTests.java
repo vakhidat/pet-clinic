9ad2f0bebae3c45bc66d6209bc60a8b8281ee9f0
Vakhidat Doskozhina <vakhidatm@gmail.com>
2020-01-18 01:14:56 +0600

    Initial Project add

---
+++ src/test/java/com/vakhidat/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vakhidat.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
